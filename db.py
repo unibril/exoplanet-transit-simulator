96ffaff8231dfabe5a8ca070bb47226c9faa1807
Unibril Karki <unibrilkarki@gmail.com>
2026-04-06 19:54:07 +0545

    database connection and creation

--- db.py
+++ db.py
@@ -1,27 +1,20 @@
 import mysql.connector
-import requests
+def database_connection():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",    
+    )   
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS space_theme")
+    cursor.execute("USE space_theme")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS PLANETS(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                planet_name VARCHAR(255),
+                size_planet float,
+                size_sun float,
+                semi_major_axis float,
+                time_orbit float
+    )""")
+    return conn
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",    
-)   
-cursor = conn.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS space_theme")
-cursor.execute("USE space_theme")
-cursor.execute("""CREATE TABLE IF NOT EXISTS PLANETS(
-               id INT AUTO_INCREMENT PRIMARY KEY,
-               planet_name VARCHAR(255),
-               size_planet float,
-               size_sun float,
-               semi_major_axis float,
-               
-               time_orbit float
-)""")
-
-
-
-
-
-cursor.close()
-conn.close()
